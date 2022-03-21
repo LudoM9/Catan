28c136dde6771a57483f938b8c57e12369bd4a63
LudoM9 <ludovic.mustiere@ensta-bretagne.org>
2022-03-21 09:51:47 +0100

    Implémentation des premières classes

---
+++ Joueur.py
@@ -0,0 +1,12 @@
+
+class Joueur():
+    def __init__(self):
+        self.nom = ""
+        self.numero = 0
+        self.main = []
+        self.carteDev = []
+        self.pointsVictoire = 0
+        self.colonies = []
+        self.villes = []
+        self.routes = []
+
